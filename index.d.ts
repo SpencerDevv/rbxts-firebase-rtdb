5d29f1b991d8444c15ae6c2d5d3a2bbe95c91ee8
SpencerDevv <94411511+SpencerDevv@users.noreply.github.com>
2022-12-24 15:47:07 -0500

    push go brr

--- index.d.ts
+++ index.d.ts
@@ -8,36 +8,36 @@ interface RTDBInstance {
 
   /**
    * Set the value of the database at key
-   * @returns {[boolean, RequestAsyncResponse]} [success, responseInfo]
+   * @returns [success, responseInfo]
    */
   SetAsync(
     key: string,
     value: unknown | void,
     method?: string
-  ): [boolean, RequestAsyncResponse];
+  ): LuaTuple<[true, RequestAsyncResponse]> | LuaTuple<[false, void]>;
 
   /**
    * Deletes key in database
    * @return [success, responseInfo]
    */
-  DeleteAsync(key: string): [boolean, RequestAsyncResponse];
+  DeleteAsync(key: string): LuaTuple<[true, RequestAsyncResponse]> | LuaTuple<[false, void]>;
 
   /**
    * Increments the value at key by delta
    * @returns [success, responseInfo]
    */
-  IncrementAsync(key: string, delta: number): [boolean, RequestAsyncResponse];
+  IncrementAsync(key: string, delta: number): LuaTuple<[true, RequestAsyncResponse]> | LuaTuple<[false, void]>;
 
   /**
   * Updates the value at key by the result of the callback
   * @param {string} key
-  * @returns {[boolean, RequestAsyncResponse]} [success, responseInfo]
+  * @returns [success, responseInfo]
   */
   UpdateAsync(
     key: string,
     callback: (old: unknown) => unknown,
     snapshot?: unknown
-  ): [boolean, RequestAsyncResponse];
+  ): LuaTuple<[true, RequestAsyncResponse]> | LuaTuple<[false, void]>;
 
   /**
    * Updates multiple values at different keys by the result of the callback
@@ -48,7 +48,7 @@ interface RTDBInstance {
     keyValues: Map<string, unknown>,
     callbacks: Map<string, (old: unknown) => unknown>,
     snapshot?: unknown
-  ): [boolean, RequestAsyncResponse];
+  ): LuaTuple<[true, RequestAsyncResponse]> | LuaTuple<[false, void]>;
 }
 
 interface FireBase {
